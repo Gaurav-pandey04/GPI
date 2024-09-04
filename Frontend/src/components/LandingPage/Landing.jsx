5bb9592d20d7b830e61d96c2fd0954d0d7838bd0
Gaurav Pandey <gauravp041104@gmail.com>
2024-09-04 21:05:11 +0530

    Added Cta and footer in landing page

--- Frontend/src/components/LandingPage/Landing.jsx
+++ Frontend/src/components/LandingPage/Landing.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Nav from './Nav'
 import Hero from './Hero'
+import Cta from './Cta'
+import Footer from './Footer'
 
 const Landing = () => {
     return (
         <div>
             <Nav />
             <Hero/>
+            <Cta/>
+            <Footer/>
         </div>
     )
 }
